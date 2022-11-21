74b7ab4d17e6a64a9db5d0ad1a098bca39355244
杉原康大朗 <sugi@sugiharayasushitairounoMacBook-Pro.local>
2022-11-21 14:49:36 +0900

    スケジュール管理アプリ作成

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TasksApp
+module TaskApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
---
+++ db/migrate/20220913182335_add_memoto_tasks.rb
@@ -0,0 +1,5 @@
+class AddMemotoTasks < ActiveRecord::Migration[6.1]
+  def change
+    add_column :tasks, :memo, :string
+  end
+end
